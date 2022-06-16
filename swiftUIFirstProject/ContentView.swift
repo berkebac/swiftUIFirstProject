2fdba1a6481403e39142dd4ac99a554528beedfb
Berke Baç <berkebac@gmail.com>
2022-06-16 17:44:50 +0300

    Initial Commit

---
+++ swiftUIFirstProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIFirstProject
+//
+//  Created by Berke Baç on 16.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIFirstProject/swiftUIFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIFirstProjectApp.swift
+//  swiftUIFirstProject
+//
+//  Created by Berke Baç on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
